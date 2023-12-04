b701a62abec73d8523ac9088781cec655090de88
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-12-04 15:45:19 -0800

    index.py updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,8 +12,21 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def get_status():
     '''Gets the status of the API.
     '''
-    return jsonify(status='OK')
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
