31385f7ea0f65bea27a7d2fdef34e0e21a469b61
NanaAba08 <williamsnanaaba@gmail.com>
2023-12-04 14:10:42 -0800

    index file

---
+++ api/v1/views/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Contains the index view for the API.'''
+from flask import jsonify
+
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
