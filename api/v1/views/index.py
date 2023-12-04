f1eaa1b21a4b2effe5dfc673364580b55d5cfd86
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-12-04 15:38:38 -0800

    reversed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Contains the index view for the API.'''
-
 from flask import jsonify
+
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -12,22 +12,8 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """
-    Returns a JSON status
-    """
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app_views.route('/status')
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
