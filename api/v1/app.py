86ec3009c33a368c6e3b402471df80d763c8601d
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-12-04 15:40:42 -0800

    app.py reversed

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,13 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+
+from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+'''The Flask web application instance.'''
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
@@ -15,17 +18,16 @@ app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
 
 
-# Custom 404 error handler
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({"error": "Not found"}), 404
-
-# App context teardown function
 @app.teardown_appcontext
 def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
+
 if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(
         host=app_host,
         port=app_port,
